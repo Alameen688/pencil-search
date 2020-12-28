5e55860d2e3e5bf0a77b7079cc6e86f4b019ca98
Alameen688 <ogundiran12@gmail.com>
2020-12-28 02:19:04 +0100

    Add sheet import functionality

---
+++ scripts/import_sheet.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const XLSX = require('xlsx');
+const dbConn = require('../db');
+const TopicModel = require('../models/topic');
+const QuestionModel = require('../models/question');
+
+const ObjectId = mongoose.Types.ObjectId;
+
+const mapTopicTree = (topics) => {
+  const insertedTopics = {};
+  const topicsRecord = [];
+  //TODO: batch insert of 1,000 if topics array for better insertMany
+  for (const topic of topics) {
+    let path = ',';
+
+    for (const key in topic) {
+      if (key.startsWith('__EMPTY')) break; // empty columns after topic level title
+
+      const name = topic[key];
+      const nameKey = name.toLowerCase();
+      let topicId = insertedTopics[nameKey];
+
+      if (!topicId) {
+        topicId = new ObjectId();
+        insertedTopics[nameKey] = topicId;
+        topicsRecord.push({
+          _id: topicId,
+          name,
+          path: path === ',' ? null : path,
+        });
+      }
+      path += `${topicId},`;
+    }
+  }
+  return { insertedTopics, topicsRecord };
+};
+
+const mapQuestionToTopic = (insertedTopics, questions) => {
+  const questionsRecord = [];
+
+  for (const question of questions) {
+    const record = { annotations: [] };
+    for (const key in question) {
+      if (key.startsWith('__EMPTY')) break;
+      if (key.toLowerCase() === 'question number') {
+        record.no = question[key];
+        continue;
+      }
+      const annotation = question[key];
+      const annotationKey = annotation.toLowerCase();
+      record.annotations.push(insertedTopics[annotationKey]);
+    }
+    if (record.annotations.length) questionsRecord.push(record);
+  }
+  return { questionsRecord };
+};
+
+const saveTopicsToDb = async (topics) => TopicModel.insertMany(topics);
+const saveQuestionsToDb = async (questions) =>
+  QuestionModel.insertMany(questions);
+
+const importSheetToDb = async () => {
+  const workbook = XLSX.readFile('data/Questions_and_Topics.xlsx');
+
+  const topics = XLSX.utils.sheet_to_json(workbook.Sheets.Topics);
+  const { insertedTopics, topicsRecord } = mapTopicTree(topics);
+
+  const questions = XLSX.utils.sheet_to_json(workbook.Sheets.Questions);
+  const { questionsRecord } = mapQuestionToTopic(insertedTopics, questions);
+
+  await Promise.all([
+    saveTopicsToDb(topicsRecord),
+    saveQuestionsToDb(questionsRecord),
+  ]);
+};
+
+importSheetToDb()
+  .then(() => {
+    console.log('Successfully imported data');
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(`An error occured ${err.message}`);
+    process.exit(1);
+  });
