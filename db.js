f9d4fefb4e30d86438c6d9a5ca16c996df03fed2
Alameen688 <ogundiran12@gmail.com>
2020-12-28 13:11:25 +0100

    Use env based db url

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
+const config = require('./config');
 
-const database_url = 'mongodb://localhost:27017/pencil-db-test';
+const DATABASE_URL =
+  config.deploy_env === 'production'
+    ? config.db_url
+    : 'mongodb://localhost:27017/pencil-db-test';
 
-mongoose.connect(database_url, {
+mongoose
+  .connect(DATABASE_URL, {
     keepAlive: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-}).then(() => {
-    console.log("Database connection successful");
-}).catch(err => {
+  })
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
     console.log('Error connecting to database. Exiting now...', err);
     process.exit();
-});
+  });
 
 module.exports = mongoose.connection;
